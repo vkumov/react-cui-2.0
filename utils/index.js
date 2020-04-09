c8246c11f3aa0c061fecb896d39e08ca1e3e41e1
vkumov <vkumov@cisco.com>
2020-04-09 10:06:16 +0200

    fix panel

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
-export const appendClass = (c, what) => (c ? ` ${what || c}` : "");
+export const appendClass = (c, what) => {
+  if (c) {
+    if (typeof what === "function") return what();
+    return what || c;
+  }
+  return "";
+};
 
 export { eventManager, EVENTS } from "./eventManager";
